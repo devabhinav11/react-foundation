4204be7a13a5da9b0f582bae3df864fb94464ead
Abhinav <abhinav@192.168.1.3>
2024-03-21 18:03:18 +0530

    info: jsx and functional components

--- App.js
+++ App.js
@@ -10,5 +10,24 @@ const parent = React.createElement(
     React.createElement("h1", {}, "Namaste React 🚀 ")
   )
 );
+
+const Title = () => {
+  return (
+    <div>
+      <h2>This is a title.</h2>
+    </div>
+  );
+};
+// React Element
+const jsxHeading = <h1>Hello, This is a jsx heading 🚀</h1>;
+
+// React Component
+const HeadingComponent = () => (
+  <div>
+    <h1>This is a Heading made by a Functional Component 🚀</h1>
+    <Title />
+  </div>
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(<HeadingComponent />);
--- README.md
+++ README.md
@@ -15,3 +15,11 @@
     - Differential Bundling - support older browsers
     - Diagnostics
     - Tree Shaking - remove unused code
+
+# JSX
+
+    JSX is not HTML in Javascript. JSX is _html-like_ syntax _or XML-like_ syntax.
+
+    Right before the code is loaded to the browser, the bundler (in our case, Parcel) transpiles the JSX to js code with the help of Babel.
+
+    JSX Code ———> React.createElement() [babel transpiles it to this] ———> React Element (JS Object) ———> HTML Element [render function converts it to this]
